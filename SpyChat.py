6cfbb2098fa96d7ce02d9929501d22ff8aa067b9
deepikabisht <bishtdeepika65@gmail.com>
2017-10-27 21:53:46 +0530

    Started Spychat

---
+++ SpyChat.py
@@ -0,0 +1,5 @@
+print 'Let\'s get started'
+spy_name = raw_input("What is your name ")
+spy_salutation = raw_input("What should we call you (Mr. or Ms.)?")
+spy_name = spy_salutation + " " + spy_name
+print "Welcome " + spy_name + " Glad to have you back with us "
