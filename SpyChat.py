68392d24e0d47f32e12ee0dc7863f1ff0ecaa280
deepikabisht <bishtdeepika65@gmail.com>
2017-10-29 22:06:59 +0530

    Spy name is valid or not

--- SpyChat.py
+++ SpyChat.py
@@ -1,5 +1,9 @@
 print 'Let\'s get started'
-spy_name = raw_input("What is your name ")
-spy_salutation = raw_input("What should we call you (Mr. or Ms.)?")
-spy_name = spy_salutation + " " + spy_name
-print "Welcome " + spy_name + " Glad to have you back with us "
+spy_name = raw_input("Welcome to spy chat, you must tell me your spy name first: ")
+if len(spy_name) > 0:
+    print 'Welcome ' + spy_name + '. Glad to have you back with us.'
+    spy_salutation = raw_input("Should I call you Mister or Miss?: ")
+    spy_name = spy_salutation + " " + spy_name
+    print "Alright " + spy_salutation + " " + spy_name + ". I'd like to know a little bit more about you before we proceed..."
+else :
+    print  "A spy needs to have a valid name. Try again please."
