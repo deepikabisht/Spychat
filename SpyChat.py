8aa5ffe46e223eb0216ce957f13bbea245539c36
deepikabisht <bishtdeepika65@gmail.com>
2017-11-04 08:00:19 +0530

    Updated everything with dictionary

--- SpyChat.py
+++ SpyChat.py
@@ -1,12 +1,12 @@
-from spy_details import spy_name,spy_salutation,spy_age,spy_rating
+from spy_details import spy
 
 print 'Let\'s get started'
 
 STATUS_MESSAGES=["Busy","At work"]
-response= raw_input("Do you  want to continue as "+spy_salutation+" "+spy_name+ " (Y/N)?")
+response= raw_input("Do you  want to continue as "+spy['salutation']+" "+spy['name']+ " (Y/N)?")
 
 
-def start_chat(spy_name,spy_age,spy_rating):
+def start_chat(spy):
     show_menu=True
     current_status_message = None
     while show_menu==True:
@@ -43,40 +43,40 @@ def add_status(current_status_message):
 
 if response.upper()=="Y":
     print "App has started"
-    start_chat(spy_name,spy_age,spy_rating)
+    start_chat(spy)
 else:
 
-    spy_name = raw_input("Welcome to spy chat, you must tell me your spy name first: ")
-    if len(spy_name) > 0:
-        print 'Welcome ' + spy_name + '. Glad to have you back with us.'
-        spy_salutation = raw_input("Should I call you Mister or Miss?: ")
-        spy_name = spy_salutation + " " + spy_name
-        print "Alright " + spy_salutation + " " + spy_name + ". I'd like to know a little bit more about you before we proceed..."
+    spy['name'] = raw_input("Welcome to spy chat, you must tell me your spy name first: ")
+    if len(spy['name']) > 0:
+        print 'Welcome ' + spy['name'] + '. Glad to have you back with us.'
+        spy['salutation'] = raw_input("Should I call you Mister or Miss?: ")
+        spy['name'] = spy['salutation'] + " " + spy['name']
+        print "Alright " + spy['salutation'] + " " + spy['name'] + ". I'd like to know a little bit more about you before we proceed..."
     else :
         print  "A spy needs to have a valid name. Try again please."
 
-    spy_age= 0
-    spy_rating= 0.0
-    spy_is_online = False
-    spy_age= (int)(raw_input(" What is your age? "))
+    spy['age']= 0
+    spy['rating']= 0.0
+    spy['is_online'] = False
+    spy['age']= (int)(raw_input(" What is your age? "))
 
-    if spy_age>12 and spy_age<50:
-        spy_rating= (float)(raw_input(" what is your spy rating "))
-        if spy_rating > 4.5:
+    if spy['age']>12 and spy['age']<50:
+        spy['rating']= (float)(raw_input(" what is your spy rating "))
+        if spy['rating'] > 4.5:
             print 'Great ace!'
-        elif spy_rating > 3.5 and spy_rating <= 4.5:
+        elif spy['rating'] > 3.5 and spy['rating'] <= 4.5:
             print 'You are one of the good ones.'
-        elif spy_rating >= 2.5 and spy_rating <= 3.5:
+        elif spy['rating'] >= 2.5 and spy['rating'] <= 3.5:
             print 'You can always do better'
         else:
             print 'We can always use somebody to help in the office.'
-        spy_is_online = True
-        print "Authentication complete. Welcome " + spy_name + " age: " + (str)(spy_age) + " and rating of: " + (str)(
-        spy_rating) + " Proud to have you onboard"
+        spy['is_online'] = True
+        print "Authentication complete. Welcome " + spy['name'] + " age: " + (str)(spy['age']) + " and rating of: " + (str)(
+            spy['rating']) + " Proud to have you onboard"
 
     else:
         print "Please enter a vlaid age"
-    start_chat(spy_name, spy_age, spy_rating)
+    start_chat(spy)
 
 
 
