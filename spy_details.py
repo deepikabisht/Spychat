91fc5db6e6d0c710e6656d7dee52177290555e82
deepikabisht <bishtdeepika65@gmail.com>
2017-11-06 21:34:13 +0530

    formated time

--- spy_details.py
+++ spy_details.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import time
 
 class Spy:
     def __init__(self, name, salutation, age, rating):
@@ -13,7 +14,9 @@ class Spy:
 class ChatMessage:
     def __init__(self,message,sent_by_me):
         self.message=message
-        self.time=datetime.now()
+        t = datetime.now()
+        t = time.mktime(t.timetuple())
+        self.time=time.strftime("%I:%M %p",time.localtime(t))
         self.sent_by_me=sent_by_me
 
 spy= Spy("Deepika","Ms.",21,5)
@@ -22,3 +25,5 @@ friend_two = Spy('Mata Hari', 'Ms.', 21, 4.39)
 friend_three = Spy('No', 'Dr.', 37, 4.95)
 
 friends = [friend_one, friend_two, friend_three]
+chat= ChatMessage("hello",True)
+chats=[]
